1fe7367d317c842de6d6e2693070d944d94503c6
Karan Khosla <Karan_Khosla@outlook.com>
2015-12-10 22:53:50 +0530

    Added test directory and skeleton file

---
+++ test/sorting/InsertionSortTest.java
@@ -0,0 +1,28 @@
+package sorting;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * File Author Karan on 12/10/15.
+ */
+public class InsertionSortTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testSort() throws Exception {
+
+    }
+}
